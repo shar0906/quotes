d4417214ab8c668457843b1fd23bd37f3d21cf02
mauro <maurop123@gmail.com>
2019-08-11 22:17:58 -0400

    add pwa notification component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <PwaNotification />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PwaNotification from './components/PwaNotification.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    PwaNotification,
   }
 }
 </script>
