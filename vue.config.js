438e997da8e723c4cb6742c2216292faf4cb123b
mauro <maurop123@gmail.com>
2019-08-17 10:58:30 -0400

    remove pwa option in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   outputDir: 'docs',
   publicPath: '/quotes/',
-  pwa: {
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-        swSrc: 'src/service-worker.js'
-    },
-    themeColor: '#1da025'
-  },
+  // pwa: {
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //       swSrc: 'src/service-worker.js'
+  //   },
+  //   themeColor: '#1da025'
+  // },
 }
